b3d188f3e4412ecc47c9c1e069a8acb9f3453d74
Mark Morgan <mark.morgan@developer.io>
2015-12-07 23:30:23 +0000

    added date and timezone utilities

---
+++ src/Utility/Date.php
@@ -0,0 +1,447 @@
+<?php
+
+/**
+ * District5 - Date
+ *
+ * @copyright District5
+ *
+ * @author Mark Morgan <mark.morgan@district5.co.uk>
+ * @link https://www.district5.co.uk
+ *
+ * @license This software and associated documentation (the "Software") may not be
+ * used, copied, modified, distributed, published or licensed to any 3rd party
+ * without the written permission of District5 or its author.
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all licensed copies of the Software.
+ *
+ */
+namespace District5\Utility;
+
+/**
+ * Date
+ *
+ * A utility for working with Dates
+ *
+ * @author Mark Morgan
+ *
+ */
+class Date
+{
+
+    /**
+     * Calculates the number of days in the given month and year
+     *
+     * @param int $month The month
+     * @param int $year The year (Optional) [Default: current year]
+     *
+     * @return int The number of days in the given month of the given year
+     */
+    public static function CalculateDaysInMonth($month, $year = null)
+    {
+        if (null === $year)
+        {
+            $now = new \DateTime();
+            $year = date('Y', $now->getTimestamp());
+        }
+
+        return cal_days_in_month(CAL_GREGORIAN, $month, $year);
+    }
+
+    /**
+     * Calculates how many days are left in a month given the current day, the month and year to check for
+     *
+     * @param int $currentDay The day to check from
+     * @param int $month The month
+     * @param int $year The year (Optional) [Default: current year]
+     *
+     * @return int The number days left in the given month
+     */
+    public static function CalculateDaysLeftInGivenMonth($currentDay, $month, $year = null)
+    {
+        $daysInMonth = static::CalculateDaysInMonth($month, $year);
+
+        if ($currentDay > $daysInMonth)
+            throw new \InvalidArgumentException('Invalid number of days, number of days exceeds the number of days in the given month');
+
+        return $daysInMonth - $currentDay;
+    }
+
+    /**
+     * Converts a Unix timestamp from Utc to an AARK supported timezone with the given id
+     *
+     * @param int|string $utcTimestamp The Utc timestamp
+     * @param string $toTimezoneId The AARK timezone id
+     *
+     * @return int The converted timestamp
+     */
+    public static function ConvertFromUtc($utcTimestamp, $toTimezoneId)
+    {
+        $timezone = Timezone::GetTimezone($toTimezoneId);
+        if (null === $timezone)
+            throw new \InvalidArgumentException('Invalid timezoneId specified in Date::ConvertFromUtc()');
+
+        $hourOffset = $timezone['utcOffset'];
+
+        // TODO: check this should have the inner strtotime(), if its already a timestamp it shouldn't be needed
+        if ($hourOffset >= 0)
+            return strtotime(date("Y-m-d H:i:s", strtotime($utcTimestamp)) . " +" . $hourOffset . " hours");
+        else
+            return strtotime(date("Y-m-d H:i:s", strtotime($utcTimestamp)) . " -" . abs($hourOffset) . " hours");
+    }
+
+    /**
+     * Converts a timestamp in a timezone with the given id to a Utc Unix timestamp
+     *
+     * @param int|string $timezoneTimestamp The current timezone timestamp
+     * @param string $fromTimezoneId The AARK timezone id
+     *
+     * @return int The converted timestamp
+     */
+    public static function ConvertToUtc($timezoneTimestamp, $fromTimezoneId)
+    {
+        $timezone = Timezone::GetTimezone($fromTimezoneId);
+        if (null === $timezone)
+            throw new \InvalidArgumentException('Invalid timezoneId specified in Date::ConvertToUtc()');
+
+        $hourOffset = $timezone['utcOffset'];
+
+        if ($hourOffset >= 0)
+            return strtotime(date("Y-m-d H:i:s", strtotime($timezoneTimestamp)) . " -" . $hourOffset . " hours");
+        else
+            return strtotime(date("Y-m-d H:i:s", strtotime($timezoneTimestamp)) . " +" . abs($hourOffset) . " hours");
+    }
+
+    /**
+     * Gets a DateTime of a given DateTime plus the number of seconds specified
+     *
+     * @param \DateTime $date The DateTime to base calculations from
+     * @param int $x The number of seconds
+     *
+     * @return \DateTime A DateTime of the given DateTime plus the number of seconds specified
+     */
+    public static function DatePlusXSeconds(\DateTime $date, $x)
+    {
+        return static::_DatePlusXTimePeriod($date, $x, 'seconds');
+    }
+
+    /**
+     * Gets a DateTime of a given DateTime plus the number of minutes specified
+     *
+     * @param \DateTime $date The DateTime to base calculations from
+     * @param int $x The number of minutes
+     *
+     * @return \DateTime A DateTime of the given DateTime plus the number of minutes specified
+     */
+    public static function DatePlusXMinutes(\DateTime $date, $x)
+    {
+        return static::_DatePlusXTimePeriod($date, $x, 'minutes');
+    }
+
+    /**
+     * Gets a DateTime of a given DateTime plus the number of hours specified
+     *
+     * @param \DateTime $date The DateTime to base calculations from
+     * @param int $x The number of hours
+     *
+     * @return \DateTime A DateTime of the given DateTime plus the number of hours specified
+     */
+    public static function DatePlusXHours(\DateTime $date, $x)
+    {
+        return static::_DatePlusXTimePeriod($date, $x, 'hours');
+    }
+
+    /**
+     * Gets a DateTime of a given DateTime plus the number of days specified
+     *
+     * @param \DateTime $date The DateTime to base calculations from
+     * @param int $x The number of days
+     *
+     * @return \DateTime A DateTime of the given DateTime plus the number of days specified
+     */
+    public static function DatePlusXDays(\DateTime $date, $x)
+    {
+        return static::_DatePlusXTimePeriod($date, $x, 'days');
+    }
+
+    /**
+     * Gets a DateTime of a given DateTime plus the number of months specified
+     *
+     * @param \DateTime $date The DateTime to base calculations from
+     * @param int $x The number of months
+     *
+     * @return \DateTime A DateTime of the given DateTime plus the number of months specified
+     */
+    public static function DatePlusXMonths(\DateTime $date, $x)
+    {
+        return static::_DatePlusXTimePeriod($date, $x, 'months');
+    }
+
+    /**
+     * Gets a DateTime of a given DateTime plus the number of years specified
+     *
+     * @param \DateTime $date The DateTime to base calculations from
+     * @param int $x The number of years
+     *
+     * @return \DateTime A DateTime of the given DateTime plus the number of years specified
+     */
+    public static function DatePlusXYears(\DateTime $date, $x)
+    {
+        return static::_DatePlusXTimePeriod($date, $x, 'years');
+    }
+
+    /**
+     * Gets a DateTime of a given DateTime plus the number of specified interval periods
+     *
+     * @param \DateTime $date The DateTime to base calculations from
+     * @param int $x The number of interval periods
+     * @param string $timePeriod The time period to use for intervals
+     *
+     * @return \DateTime A DateTime of the given DateTime plus the number of time intervals specified
+     */
+    protected static function _DatePlusXTimePeriod(\DateTime $date, $x, $timePeriod)
+    {
+        $endDate = strtotime(date("Y-m-d H:i:s", $date->getTimestamp()) . " +" . $x . " " . $timePeriod);
+
+        $dt = new \DateTime();
+        $dt->setTimestamp($endDate);
+
+        return $dt;
+    }
+
+    /**
+     * Gets the 2 character english suffix for a number
+     *
+     * @param int|string $number The number to generate a suffix for
+     *
+     * @return string The 2 digit english suffix ('st', 'nd', 'rd' or 'th')
+     */
+    public static function GetNumberSuffix($number)
+    {
+        $lastDigit = substr((string)($number), -1);
+        switch ($lastDigit)
+        {
+            case "1":
+                return 'st';
+            case "2":
+                return 'nd';
+            case "3":
+                return 'rd';
+            default:
+                return 'th';
+        }
+    }
+
+    /**
+     * Checks whether dateStr1 is after dateStr2 utilising strtotime() to calculate dates
+     *
+     * @param string $dateStr1
+     * @param string $dateStr2
+     *
+     * @return bool True if dateStr1 is after dateStr2, false otherwise
+     */
+    public static function IsDateAfterDate($dateStr1, $dateStr2)
+    {
+        return strtotime($dateStr1) > strtotime($dateStr2);
+    }
+
+    /**
+     * Checks whether dateStr1 is before dateStr2 utilising strtotime() to calculate dates
+     *
+     * @param string $dateStr1
+     * @param string $dateStr2
+     *
+     * @return bool True if dateStr1 is before dateStr2, false otherwise
+     */
+    public static function IsDateBeforeDate($dateStr1, $dateStr2)
+    {
+        return strtotime($dateStr1) < strtotime($dateStr2);
+    }
+
+    /**
+     * Gets a DateTime of the current time plus the number of seconds specified
+     *
+     * @param int $x The number of seconds
+     *
+     * @return \DateTime A DateTime of the current time plus the number of seconds specified
+     */
+    public static function NowPlusXSeconds($x)
+    {
+        return static::_NowPlusXTimePeriod($x, 'seconds');
+    }
+
+    /**
+     * Gets a DateTime of the current time plus the number of minutes specified
+     *
+     * @param int $x The number of minutes
+     *
+     * @return \DateTime A DateTime of the current time plus the number of minutes specified
+     */
+    public static function NowPlusXMinutes($x)
+    {
+        return static::_NowPlusXTimePeriod($x, 'minutes');
+    }
+
+    /**
+     * Gets a DateTime of the current time plus the number of hours specified
+     *
+     * @param int $x The number of hours
+     *
+     * @return \DateTime A DateTime of the current time plus the number of hours specified
+     */
+    public static function NowPlusXHours($x)
+    {
+        return static::_NowPlusXTimePeriod($x, 'hours');
+    }
+
+    /**
+     * Gets a DateTime of the current time plus the number of days specified
+     *
+     * @param int $x The number of days
+     *
+     * @return \DateTime A DateTime of the current time plus the number of days specified
+     */
+    public static function NowPlusXDays($x)
+    {
+        return static::_NowPlusXTimePeriod($x, 'days');
+    }
+
+    /**
+     * Gets a DateTime of the current time plus the number of months specified
+     *
+     * @param int $x The number of months
+     *
+     * @return \DateTime A DateTime of the current time plus the number of months specified
+     */
+    public static function NowPlusXMonths($x)
+    {
+        return static::_NowPlusXTimePeriod($x, 'months');
+    }
+
+    /**
+     * @param $x
+     * @param $timePeriod
+     * @return \DateTime
+     */
+    protected static function _NowPlusXTimePeriod($x, $timePeriod)
+    {
+        $date = date("Y-m-d H:i:s");
+
+        $date = strtotime(date("Y-m-d H:i:s", strtotime($date)) . " +" . $x . "  " . $timePeriod);
+
+        $dt = new \DateTime();
+        $dt->setTimestamp($date);
+
+        return $dt;
+    }
+
+    /**
+     * Gets a DateTime given a string representation
+     *
+     * @param String $dateString The string should be in the format "yyyy-mm-dd hh:mm:ss"
+     *
+     * @return \DateTime A DateTime of the specified date string
+     */
+    public static function SpecificDate($dateString)
+    {
+        $dt = new \DateTime();
+        $dt->setTimestamp(strtotime($dateString));
+
+        return $dt;
+    }
+
+    /**
+     * Gets a string representation of a non-leading zero day of a DateTime
+     *
+     * @param \DateTime $date
+     *
+     * @return string
+     */
+    public static function ToDayString($date)
+    {
+        return self::_DateToString($date, 'j');
+    }
+
+    /**
+     * Gets a string representation of a leading zero day followed by 3 letter month (e.g. 16 Apr) of a DateTime
+     *
+     * @param \DateTime $date
+     *
+     * @return string
+     */
+    public static function ToDayMonthString($date)
+    {
+        return self::_DateToString($date, 'd M');
+    }
+
+    /**
+     * Gets a string representation of a year month day of a DateTime, in the format yyyy-mm-dd
+     *
+     * @param \DateTime $date
+     *
+     * @return string
+     */
+    public static function ToYearMonthDayString($date)
+    {
+        return self::_DateToString($date, 'Y-d-m');
+    }
+
+    /**
+     * Gets a string representation of a non-leading zero month of a DateTime
+     *
+     * @param \DateTime $date
+     *
+     * @return string
+     */
+    public static function ToMonthNumericString($date)
+    {
+        return self::_DateToString($date, 'n');
+    }
+
+    /**
+     * Gets a string representation of a full 4 digit year
+     *
+     * @param \DateTime $date
+     *
+     * @return string
+     */
+    public static function ToYearString($date)
+    {
+        return self::_DateToString($date, 'Y');
+    }
+
+    public static function ToReallyPrettyDateString($date)
+    {
+        return self::_DateToString($date, 'l jS F, Y');
+    }
+
+    /**
+     * Gets a 'pretty' human readable string representation of a DateTime
+     *
+     * @param \DateTime $date
+     *
+     * @return string The DateTime formatted in a human readable string
+     */
+    public static function ToPrettyDateTimeString($date)
+    {
+        return self::_DateToString($date, 'D jS M, Y (H:i)');
+    }
+
+    public static function ToTime24Hour($date)
+    {
+        return self::_DateToString($date, 'H:i');
+    }
+
+    /**
+     * Formats a date as a string with the given format
+     *
+     * @param \DateTime $date The DateTime to format
+     * @param string $format The format
+     *
+     * @return string The formatted date string
+     */
+    protected static function _DateToString($date, $format)
+    {
+        return date($format, $date->getTimestamp());
+    }
+}
---
+++ src/Utility/Timezone.php
@@ -0,0 +1,157 @@
+<?php
+
+/**
+ * District5 - Date
+ *
+ * @copyright District5
+ *
+ * @author Mark Morgan <mark.morgan@district5.co.uk>
+ * @link https://www.district5.co.uk
+ *
+ * @license This software and associated documentation (the "Software") may not be
+ * used, copied, modified, distributed, published or licensed to any 3rd party
+ * without the written permission of District5 or its author.
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all licensed copies of the Software.
+ *
+ */
+namespace District5\Utility;
+
+/**
+ * Timezone
+ *
+ * A utility for working with Timezones
+ *
+ * @author Mark Morgan
+ *
+ */
+class Timezone
+{
+
+    protected static $_options = array(
+        '1' => array('id' => '1', 'utcOffset' => -12, 'label' => '(GMT -12:00) International Date Line West'),
+        '2' => array('id' => '2', 'utcOffset' => -11, 'label' => '(GMT -11:00) Midway Island, Samoa'),
+        '3' => array('id' => '3', 'utcOffset' => -10, 'label' => '(GMT -10:00) Hawaii'),
+        '4' => array('id' => '4', 'utcOffset' => -9, 'label' => '(GMT -09:00) Alaska'),
+        '5' => array('id' => '5', 'utcOffset' => -8, 'label' => '(GMT -08:00) Pacific Time (US & Canada)'),
+        '6' => array('id' => '6', 'utcOffset' => -8, 'label' => '(GMT -08:00) Tijuana, Baja California'),
+        '7' => array('id' => '7', 'utcOffset' => -7, 'label' => '(GMT -07:00) Arizona'),
+        '8' => array('id' => '8', 'utcOffset' => -7, 'label' => '(GMT -07:00) Chihuahua, La Paz, Mazatlan'),
+        '9' => array('id' => '9', 'utcOffset' => -7, 'label' => '(GMT -07:00) Mountain Time (US & Canada)'),
+        '10' => array('id' => '10', 'utcOffset' => -6, 'label' => '(GMT -06:00) Central America'),
+        '11' => array('id' => '11', 'utcOffset' => -6, 'label' => '(GMT -06:00) Central Time (US & Canada)'),
+        '12' => array('id' => '12', 'utcOffset' => -6, 'label' => '(GMT -06:00) Guadalajara, Mexico City, Monterrey'),
+        '13' => array('id' => '13', 'utcOffset' => -6, 'label' => '(GMT -06:00) Saskatchewan'),
+        '14' => array('id' => '14', 'utcOffset' => -5, 'label' => '(GMT -05:00) Bogota, Lima, Quito, Rio Branco'),
+        '15' => array('id' => '15', 'utcOffset' => -5, 'label' => '(GMT -05:00) Eastern Time (US & Canada)'),
+        '16' => array('id' => '16', 'utcOffset' => -5, 'label' => '(GMT -05:00) Indiana (East)'),
+        '17' => array('id' => '17', 'utcOffset' => -4, 'label' => '(GMT -04:00) Atlantic Time (Canada)'),
+        '18' => array('id' => '18', 'utcOffset' => -4, 'label' => '(GMT -04:00) Caracas, La Paz'),
+        '19' => array('id' => '19', 'utcOffset' => -4, 'label' => '(GMT -04:00) Manaus'),
+        '20' => array('id' => '20', 'utcOffset' => -4, 'label' => '(GMT -04:00) Santiago'),
+        '21' => array('id' => '21', 'utcOffset' => -3.5, 'label' => '(GMT -03:30) Newfoundland'),
+        '22' => array('id' => '22', 'utcOffset' => -3, 'label' => '(GMT -03:00) Brasilia'),
+        '23' => array('id' => '23', 'utcOffset' => -3, 'label' => '(GMT -03:00) Buenos Aires, Georgetown'),
+        '24' => array('id' => '24', 'utcOffset' => -3, 'label' => '(GMT -03:00) Greenland'),
+        '25' => array('id' => '25', 'utcOffset' => -3, 'label' => '(GMT -03:00) Montevideo'),
+        '26' => array('id' => '26', 'utcOffset' => -2, 'label' => '(GMT -02:00) Mid-Atlantic'),
+        '27' => array('id' => '27', 'utcOffset' => -1, 'label' => '(GMT -01:00) Cape Verde Is.'),
+        '28' => array('id' => '28', 'utcOffset' => -1, 'label' => '(GMT -01:00) Azores'),
+        '29' => array('id' => '29', 'utcOffset' => 0, 'label' => '(GMT +00:00) Casablanca, Monrovia, Reykjavik'),
+        '30' => array('id' => '30', 'utcOffset' => 0, 'label' => '(GMT +00:00) Greenwich Mean Time : Dublin, Edinburgh, Lisbon, London'),
+        '31' => array('id' => '31', 'utcOffset' => 1, 'label' => '(GMT +01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna'),
+        '32' => array('id' => '32', 'utcOffset' => 1, 'label' => '(GMT +01:00) Belgrade, Bratislava, Budapest, Ljubljana, Prague'),
+        '33' => array('id' => '33', 'utcOffset' => 1, 'label' => '(GMT +01:00) Brussels, Copenhagen, Madrid, Paris'),
+        '34' => array('id' => '34', 'utcOffset' => 1, 'label' => '(GMT +01:00) Sarajevo, Skopje, Warsaw, Zagreb'),
+        '35' => array('id' => '35', 'utcOffset' => 1, 'label' => '(GMT +01:00) West Central Africa'),
+        '36' => array('id' => '36', 'utcOffset' => 2, 'label' => '(GMT +02:00) Amman'),
+        '37' => array('id' => '37', 'utcOffset' => 2, 'label' => '(GMT +02:00) Athens, Bucharest, Istanbul'),
+        '38' => array('id' => '38', 'utcOffset' => 2, 'label' => '(GMT +02:00) Beirut'),
+        '39' => array('id' => '39', 'utcOffset' => 2, 'label' => '(GMT +02:00) Cairo'),
+        '40' => array('id' => '40', 'utcOffset' => 2, 'label' => '(GMT +02:00) Harare, Pretoria'),
+        '41' => array('id' => '41', 'utcOffset' => 2, 'label' => '(GMT +02:00) Helsinki, Kyiv, Riga, Sofia, Tallinn, Vilnius'),
+        '42' => array('id' => '42', 'utcOffset' => 2, 'label' => '(GMT +02:00) Jerusalem'),
+        '43' => array('id' => '43', 'utcOffset' => 2, 'label' => '(GMT +02:00) Minsk'),
+        '44' => array('id' => '44', 'utcOffset' => 2, 'label' => '(GMT +02:00) Windhoek'),
+        '45' => array('id' => '45', 'utcOffset' => 3, 'label' => '(GMT +03:00) Kuwait, Riyadh, Baghdad'),
+        '46' => array('id' => '46', 'utcOffset' => 3, 'label' => '(GMT +03:00) Moscow, St. Petersburg, Volgograd'),
+        '47' => array('id' => '47', 'utcOffset' => 3, 'label' => '(GMT +03:00) Nairobi'),
+        '48' => array('id' => '48', 'utcOffset' => 3, 'label' => '(GMT +03:00) Tbilisi'),
+        '49' => array('id' => '49', 'utcOffset' => 3.5, 'label' => '(GMT +03:30) Tehran'),
+        '50' => array('id' => '50', 'utcOffset' => 4, 'label' => '(GMT +04:00) Abu Dhabi, Muscat'),
+        '51' => array('id' => '51', 'utcOffset' => 4, 'label' => '(GMT +04:00) Baku'),
+        '52' => array('id' => '52', 'utcOffset' => 4, 'label' => '(GMT +04:00) Yerevan'),
+        '53' => array('id' => '53', 'utcOffset' => 4.5, 'label' => '(GMT +04:30) Kabul'),
+        '54' => array('id' => '54', 'utcOffset' => 5, 'label' => '(GMT +05:00) Yekaterinburg'),
+        '55' => array('id' => '55', 'utcOffset' => 5, 'label' => '(GMT +05:00) Islamabad, Karachi, Tashkent'),
+        '56' => array('id' => '56', 'utcOffset' => 5.5, 'label' => '(GMT +05:30) Sri Jayawardenapura'),
+        '57' => array('id' => '57', 'utcOffset' => 5.5, 'label' => '(GMT +05:30) Chennai, Kolkata, Mumbai, New Delhi'),
+        '58' => array('id' => '58', 'utcOffset' => 5.75, 'label' => '(GMT +05:45) Kathmandu'),
+        '59' => array('id' => '59', 'utcOffset' => 6, 'label' => '(GMT +06:00) Almaty, Novosibirsk'),
+        '60' => array('id' => '60', 'utcOffset' => 6, 'label' => '(GMT +06:00) Astana, Dhaka'),
+        '61' => array('id' => '61', 'utcOffset' => 6.5, 'label' => '(GMT +06:30) Yangon (Rangoon)'),
+        '62' => array('id' => '62', 'utcOffset' => 7, 'label' => '(GMT +07:00) Bangkok, Hanoi, Jakarta'),
+        '63' => array('id' => '63', 'utcOffset' => 7, 'label' => '(GMT +07:00) Krasnoyarsk'),
+        '64' => array('id' => '64', 'utcOffset' => 8, 'label' => '(GMT +08:00) Beijing, Chongqing, Hong Kong, Urumqi'),
+        '65' => array('id' => '65', 'utcOffset' => 8, 'label' => '(GMT +08:00) Kuala Lumpur, Singapore'),
+        '66' => array('id' => '66', 'utcOffset' => 8, 'label' => '(GMT +08:00) Irkutsk, Ulaan Bataar'),
+        '67' => array('id' => '67', 'utcOffset' => 8, 'label' => '(GMT +08:00) Perth'),
+        '68' => array('id' => '68', 'utcOffset' => 8, 'label' => '(GMT +08:00) Taipei'),
+        '69' => array('id' => '69', 'utcOffset' => 9, 'label' => '(GMT +09:00) Osaka, Sapporo, Tokyo'),
+        '70' => array('id' => '70', 'utcOffset' => 9, 'label' => '(GMT +09:00) Seoul'),
+        '71' => array('id' => '71', 'utcOffset' => 9, 'label' => '(GMT +09:00) Yakutsk'),
+        '72' => array('id' => '72', 'utcOffset' => 9.5, 'label' => '(GMT +09:30) Adelaide'),
+        '73' => array('id' => '73', 'utcOffset' => 9.5, 'label' => '(GMT +09:30) Darwin'),
+        '74' => array('id' => '74', 'utcOffset' => 10, 'label' => '(GMT +10:00) Brisbane'),
+        '75' => array('id' => '75', 'utcOffset' => 10, 'label' => '(GMT +10:00) Canberra, Melbourne, Sydney'),
+        '76' => array('id' => '76', 'utcOffset' => 10, 'label' => '(GMT +10:00) Hobart'),
+        '77' => array('id' => '77', 'utcOffset' => 10, 'label' => '(GMT +10:00) Guam, Port Moresby'),
+        '78' => array('id' => '78', 'utcOffset' => 10, 'label' => '(GMT +10:00) Vladivostok'),
+        '79' => array('id' => '79', 'utcOffset' => 11, 'label' => '(GMT +11:00) Magadan, Solomon Is., New Caledonia'),
+        '80' => array('id' => '80', 'utcOffset' => 12, 'label' => '(GMT +12:00) Auckland, Wellington'),
+        '81' => array('id' => '81', 'utcOffset' => 12, 'label' => '(GMT +12:00) Fiji, Kamchatka, Marshall Is.'),
+        '82' => array('id' => '82', 'utcOffset' => 13, 'label' => '(GMT +13:00) Nuku\'alofa')
+    );
+
+    /**
+     * Gets a specific timezone given the timezone id
+     *
+     * @param string $timezoneId The timezone id
+     *
+     * @return array The details of the timezone with the given id in an array
+     * of $timezoneId => array(id, utcOffset, label), null if no timezone details found
+     */
+    public static function GetTimezone($timezoneId)
+    {
+        if (!array_key_exists($timezoneId, static::$_options))
+            return null;
+
+        return static::$_options[$timezoneId];
+    }
+
+    /**
+     * Gets all supported timezones
+     *
+     * @return array All the timezones in an array of $timezoneId => array(id, utcOffset, label)
+     */
+    public static function GetTimezones()
+    {
+        return static::$_options;
+    }
+
+    /**
+     * Checks whether timezone id is valid
+     *
+     * @param string $id The timezone id
+     *
+     * @return bool True if the id is a valid timezone id, false otherwise
+     */
+    public static function IsValidTimezoneId($id)
+    {
+        if (!array_key_exists($id, static::$_options))
+            return false;
+
+        return true;
+    }
+}
